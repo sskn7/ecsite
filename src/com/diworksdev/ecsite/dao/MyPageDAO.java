1ef686c05dbf4333e3e06fd44fbba37296691f5e
kana sasazawa <kana.yellow7@gmail.com>
2025-11-15 22:54:30 +0900

    fix #商品削除ができなかったのを解消

--- src/com/diworksdev/ecsite/dao/MyPageDAO.java
+++ src/com/diworksdev/ecsite/dao/MyPageDAO.java
@@ -51,8 +51,8 @@ public class MyPageDAO {
          }
 
 	    public int buyItemHistoryDelete(String item_transaction_id,String user_master_id) throws SQLException {
-		String sql = "DELETE FROM user_buy_item_transaction"
-				+ "WHERE item_transaction_id=?"
+		String sql = "DELETE FROM user_buy_item_transaction "
+				+ "WHERE item_transaction_id=? "
 				+ "AND user_master_id = ?";
 
 		PreparedStatement preparedStatement;
